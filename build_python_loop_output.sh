cef85e5adb3d5ef31f648a9edb06de23074cd120
Kevin P. Inscoe <kevin.inscoe@gmail.com>
2023-05-19 08:39:30 -0400

    Update build_python_loop_output.sh
    Correct image name

--- build_python_loop_output.sh
+++ build_python_loop_output.sh
@@ -5,7 +5,7 @@
 
 version="1.0"
 
-docker build -t eks_hello_world:${version} -f python_dockerfiles/python_loop_output/Dockerfile .
+docker build -t kevin_test_loop_python:${version} -f python_dockerfiles/python_loop_output/Dockerfile .
 image_id=$(docker images | grep kevin_test_loop_python | grep "${version}" | awk '{ print $3 }')
 echo "Image ID: ${image_id}"
 docker tag "${image_id}" ghcr.io/kevinpinscoe/kevin_test_loop_python:latest
