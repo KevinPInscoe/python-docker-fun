93c3e128155ab854698e424485fc50a0c4297d8c
Kevin P. Inscoe <kevin.inscoe@gmail.com>
2023-05-19 10:06:59 -0400

    Update build_python_loop_output.sh
    Change docker file location to current directory

--- build_python_loop_output.sh
+++ build_python_loop_output.sh
@@ -5,7 +5,7 @@
 
 version="1.0"
 
-docker build -t kevin_test_loop_python:${version} -f python_dockerfiles/python_loop_output/Dockerfile .
+docker build -t kevin_test_loop_python:${version} -f Dockerfile .
 image_id=$(docker images | grep kevin_test_loop_python | grep "${version}" | awk '{ print $3 }')
 echo "Image ID: ${image_id}"
 docker tag "${image_id}" ghcr.io/kevinpinscoe/kevin_test_loop_python:latest
